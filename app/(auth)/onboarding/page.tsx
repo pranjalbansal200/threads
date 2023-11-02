5d3b468c77685766cb6dab30840b17a76aecc641
Pranjal Bansal <pranjal.c@quickheal.com>
2023-11-02 17:27:56 +0530

    commented some code which is not required for now

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,19 +1,22 @@
 import AccountProfile from "@/components/forms/AccountProfile";
 import { currentUser } from "@clerk/nextjs";
+// import { redirect } from "next/navigation";
+// import { fetchUser } from "@/lib/actions/user.actions";
 
 async function page() {
     const user=await currentUser();
     if(!user) return null;
 
-    const userInfo={};
+//     const userInfo = await fetchUser(user.id);
+//   if (userInfo?.onboarded) redirect("/");
 
     const userData={
         id:user?.id,
-        objectid:userInfo?._id,
-        username: userInfo ? userInfo?.username : user.username,
-        name: userInfo ? userInfo?.name : user.firstName ?? "",
-        bio: userInfo ? userInfo?.bio : "",
-        image: userInfo ? userInfo?.image : user.imageUrl,
+        // objectid:userInfo?._id,
+        // username: userInfo ? userInfo?.username : user.username,
+        // name: userInfo ? userInfo?.name : user.firstName ?? "",
+        // bio: userInfo ? userInfo?.bio : "",
+        // image: userInfo ? userInfo?.image : user.imageUrl,
     };
 
     return(
