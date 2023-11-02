50ab8764d0d44aeae30218196dd0ac6acb2ad866
Pranjal Bansal <pranjal.c@quickheal.com>
2023-11-02 12:54:51 +0530

    This commit is a mistake

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,34 +3,36 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose"
+import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 
-export async function  updateUser(
-    userId:string,
-    username:string,
-    name:string,
-    bio:string,
-    image:string,
-    path:string,
-) : Promise<void>{
-    connectToDB();
-  try{
-    await User.findOneAndUpdate(
-        {id:userId},
+export async function updateUser({
+    userId,
+    bio,
+    name,
+    path,
+    username,
+    image,
+  }: Params): Promise<void> {
+    try {
+      connectToDB();
+  
+      await User.findOneAndUpdate(
+        { id: userId },
         {
-            username:username.toLowerCase(),
-            name,
-            bio,
-            image,
-            onboarded:true,
+          username: username.toLowerCase(),
+          name,
+          bio,
+          image,
+          onboarded: true,
         },
-        {upsert:true}
-    );
-
-    if(path==='/profile/edit'){
+        { upsert: true }
+      );
+  
+      if (path === "/profile/edit") {
         revalidatePath(path);
+      }
+    } catch (error: any) {
+      throw new Error(`Failed to create/update user: ${error.message}`);
     }
-}catch(error:any){
-    throw new Error(`Failed to create/update user: ${error.message}`)
-}
-    
-}
+  }
+  
