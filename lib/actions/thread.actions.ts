a6bcf534beef831792a822197f9de148f919c33e
Pranjal Bansal <pranjal.c@quickheal.com>
2023-11-04 19:53:15 +0530

    added Comment compnent to ensure users can reply to threads

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -72,3 +72,80 @@ export async function fetchPosts(pageNumber=1, pageSize=20){
 
             return { posts, isNext };
 }
+
+export async function fetchThreadById(id: string) {
+    connectToDB();
+  
+    try {
+      const thread = await Thread.findById(id)
+        .populate({
+          path: "author",
+          model: User,
+          select: "_id id name image",
+        }) // Populate the author field with _id and username
+        .populate({
+          path: "children", // Populate the children field
+          populate: [
+            {
+              path: "author", // Populate the author field within children
+              model: User,
+              select: "_id id name parentId image", // Select only _id and username fields of the author
+            },
+            {
+              path: "children", // Populate the children field within children
+              model: Thread, // The model of the nested children (assuming it's the same "Thread" model)
+              populate: {
+                path: "author", // Populate the author field within nested children
+                model: User,
+                select: "_id id name parentId image", // Select only _id and username fields of the author
+              },
+            },
+          ],
+        })
+        .exec();
+  
+      return thread;
+    } catch (err) {
+      console.error("Error while fetching thread:", err);
+      throw new Error("Unable to fetch thread");
+    }
+  }
+
+  export async function addCommentToThread(
+    threadId: string,
+    commentText: string,
+    userId: string,
+    path: string
+  ) {
+    connectToDB();
+
+    try {
+        // Find the original thread by its ID
+        const originalThread = await Thread.findById(threadId);
+    
+        if (!originalThread) {
+          throw new Error("Thread not found");
+        }
+    
+        // Create the new comment thread
+        const commentThread = new Thread({
+          text: commentText,
+          author: userId,
+          parentId: threadId, // Set the parentId to the original thread's ID
+        });
+    
+        // Save the comment thread to the database
+        const savedCommentThread = await commentThread.save();
+    
+        // Add the comment thread's ID to the original thread's children array
+        originalThread.children.push(savedCommentThread._id);
+    
+        // Save the updated original thread to the database
+        await originalThread.save();
+    
+        revalidatePath(path);
+      } catch (err) {
+        console.error("Error while adding comment:", err);
+        throw new Error("Unable to add comment");
+      }
+  }
