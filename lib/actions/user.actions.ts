79b6d01b55a5ad061044ac9147c54fb74415c94c
Pranjal Bansal <pranjal.c@quickheal.com>
2023-11-06 14:44:52 +0530

    created an activity page where users can check the comments on their posts

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -62,16 +62,16 @@ export async function fetchUserPosts(userId: string) {
 
     // Find all threads authored by the user with the given userId
     const threads = await User.findOne({ id: userId }).populate({
-      path: 'threads',
+      path: "threads",
       model: Thread,
       populate: 
         {
-          path: 'children',
+          path: "children",
           model: Thread,
           populate: {
-            path: 'author',
+            path: "author",
             model: User,
-            select: 'name image id', // Select the "name" and "_id" fields from the "User" model
+            select: "name image _id", // Select the "name" and "_id" fields from the "User" model
           },
         },
     });
@@ -136,6 +136,35 @@ export async function fetchUsers({
     return { users, isNext };
   } catch (error) {
     console.error("Error fetching users:", error);
+    throw error;
+  }
+}
+
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+
+    // Find all threads created by the user
+    const userThreads = await Thread.find({ author: userId });
+
+    // Collect all the child thread ids (replies) from the 'children' field of each user thread
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    }, []);
+
+    // Find and return the child threads (replies) excluding the ones created by the same user
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId }, // Exclude threads authored by the same user
+    }).populate({
+      path: "author",
+      model: User,
+      select: "name image _id",
+    });
+
+    return replies;
+  } catch (error) {
+    console.error("Error fetching replies: ", error);
     throw error;
   }
 }
